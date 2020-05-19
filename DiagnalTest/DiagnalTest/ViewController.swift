9ce817ad3cb846081b0424711b3661e45b8a7fc2
Manoj Shivhare <manojshivhare820@gmail.com>
2020-05-19 16:32:27 +0530

    first commit

---
+++ DiagnalTest/DiagnalTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiagnalTest
+//
+//  Created by Manoj Shivhare on 19/05/20.
+//  Copyright © 2020 Manoj Shivhare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
